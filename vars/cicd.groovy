11cfbfa1900572d1b97a41c621cd665be8c97494
JoyOrji1 <105261411+JoyOrji1@users.noreply.github.com>
2022-05-20 05:54:18 -0700

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,3 +11,7 @@ def newDeploy(ip,appname)
 {
   deploy adapters: [tomcat9(credentialsId: 'cd595cf8-6c78-42e2-8384-ef161cccf279', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
 }
+def newTest(path)
+{
+  sh 'java -jar "${path}"/testing.jar'
+}
