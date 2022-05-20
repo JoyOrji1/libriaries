d252e5f8722a12ba41c81be25572fa7f200e0c30
JoyOrji1 <105261411+JoyOrji1@users.noreply.github.com>
2022-05-20 04:58:50 -0700

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,13 @@
+def newGit(repo)
+{
+  git "${repo}"
+}
+
+def newMaven()
+{
+  sh 'mvn package'
+}
+def newDeploy(ip,appname)
+{
+  deploy adapters: [tomcat9(credentialsId: 'cd595cf8-6c78-42e2-8384-ef161cccf279', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
+}
